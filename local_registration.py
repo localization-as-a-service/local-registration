f1d179d87091d4104977f2c7007d8a95446e0838
Darshana Rathnayake <darshanakgr@gmail.com>
2022-11-30 19:26:21 +0800

    latency measurements

--- local_registration.py
+++ local_registration.py
@@ -1,16 +1,82 @@
 import open3d
-import numpy
+import numpy as np
+import tqdm
 import os
+import time
+
 import utils.fread as fread
-import utils.FCGF as FCGF
+import utils.helpers as helpers
 import utils.registration as registration
 
 
 def main():
-    sequence_dir = "../localization-data/data/features/exp_5/trial_1/0.03/subject-1/01"
-    sequence_ts = fread.get_timstamps(sequence_dir)
+    exec_t = [[] for _ in range(4)] # [loading pcds, downsampling, feature extraction, ransac]
     
-    source = FCGF.get_features(os.path.join(sequence_dir, f"{sequence_ts[0]}.secondary.npz"), pcd_only=True)
+    sequence_dir = "../localization-data/data/point_clouds/exp_5/trial_1/subject-1/01"
+    sequence_ts = fread.get_timstamps(sequence_dir, ext=".secondary.pcd")
+    
+    num_frames = len(sequence_ts)
+    
+    print("-> Loading point clouds...")
+        
+    local_pcds = []
+
+    for t in tqdm.trange(num_frames):
+        start_t = time.time_ns()
+        pcd = helpers.read_pcd(os.path.join(sequence_dir, f"{sequence_ts[t]}.secondary.pcd"))
+        end_t = time.time_ns()
+        exec_t[0].append(end_t - start_t)
+        
+        start_t = time.time_ns()
+        pcd = helpers.downsample(pcd, 0.05)
+        end_t = time.time_ns()
+        exec_t[1].append(end_t - start_t)
+        
+        local_pcds.append(pcd)
+        
+    print("-> Registering point clouds...")
+        
+    fpfh_feats = [registration.compute_fpfh(local_pcds[0], 0.05)]
+    local_t = [np.identity(4)]
+
+    for t in tqdm.trange(num_frames - 1):
+        start_t = time.time_ns()
+        source_fpfh = registration.compute_fpfh(local_pcds[t + 1], 0.05)
+        end_t = time.time_ns()
+        exec_t[2].append(end_t - start_t)
+        
+        start_t = time.time_ns()
+        ransac_reg = registration.ransac_feature_matching(local_pcds[t + 1], local_pcds[t], source_fpfh, fpfh_feats[t], n_ransac=4, threshold=0.05)
+        end_t = time.time_ns()
+        exec_t[3].append(end_t - start_t)
+            
+        local_t.append(ransac_reg.transformation)
+        fpfh_feats.append(source_fpfh)
+        
+    print("-> Refining trajectory transformations...")
+        
+    trajectory_t = [np.identity(4)]
+
+    for t in tqdm.trange(num_frames):
+        if t > 0:
+            trajectory_t.append(np.dot(trajectory_t[t - 1], local_t[t]))
+        local_pcds[t].transform(trajectory_t[t])
+        
+    print("-> Saving trajectory results...")
+    
+    trajectory = helpers.merge_pcds(local_pcds, 0.05)
+    open3d.io.write_point_cloud("output/trajectory.pcd", trajectory)
+    
+    del exec_t[0][0]
+    del exec_t[1][0]
+    
+    exec_t = np.array(exec_t)
+    exec_t = np.mean(exec_t, axis=0)
+    print(f"-> Execution times:")
+    print(f"\t{'Loading pcds':<20}: {exec_t[0] / 1e6} ms")
+    print(f"\t{'Downsampling':<20}: {exec_t[1] / 1e6} ms")
+    print(f"\t{'Feature extraction':<20}: {exec_t[2] / 1e6} ms")
+    print(f"\t{'RANSAC':<20}: {exec_t[3] / 1e6} ms")
     
     
 if __name__ == "__main__":
--- utils/registration.py
+++ utils/registration.py
@@ -1,49 +1,5 @@
 import open3d
-import numpy as np
-import os
-
-
-def exec_local_reg(source, target, threshold, trans_init, max_iteration=30, p2p=True):
-    if p2p:
-        estimation_method = open3d.registration.TransformationEstimationPointToPoint(False)
-    else:
-        estimation_method = open3d.registration.TransformationEstimationPointToPlane()
-        
-    return open3d.registration.registration_icp(
-        source, target, threshold, trans_init, estimation_method,
-        open3d.registration.ICPConvergenceCriteria(max_iteration=max_iteration)
-    )
-
-
-def exec_global_reg(source, target, source_feat, target_feat, n_ransac, threshold, p2p=True):
-    if p2p:
-        estimation_method = open3d.registration.TransformationEstimationPointToPoint(False)
-    else:
-        estimation_method = open3d.registration.TransformationEstimationPointToPlane()
-        
-    result = open3d.registration.registration_ransac_based_on_feature_matching(
-        source, target, source_feat, target_feat, threshold,
-        estimation_method, n_ransac,
-        [
-            open3d.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
-            open3d.registration.CorrespondenceCheckerBasedOnDistance(threshold)
-        ],
-        open3d.registration.RANSACConvergenceCriteria(4000000, 600))
-    return result
-
-
-def compute_fpff(pcd, voxel_size, down_sample=True):
-    if down_sample:
-        pcd = open3d.voxel_down_sample(pcd, voxel_size)
-
-    radius_normal = voxel_size * 2
-    open3d.geometry.estimate_normals(
-        pcd, open3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
-
-    radius_feature = voxel_size * 5
-    pcd_fpfh = open3d.registration.compute_fpfh_feature(
-        pcd, open3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
-    return pcd, pcd_fpfh
+import copy
 
 
 def describe(source, target, result, end="\n"):
@@ -53,19 +9,34 @@ def describe(source, target, result, end="\n"):
     print(f"Inlier RMSE: {result.inlier_rmse:.4f}", end=end)
 
 
-def register_fragments_local(sequence_dir, sequence_ts, t, voxel_size):
-    src_feature_file = os.path.join(sequence_dir, f"{sequence_ts[t]}.secondary.npz")
-    tgt_feature_file = os.path.join(sequence_dir, f"{sequence_ts[t + 1]}.secondary.npz")
+def compute_fpfh(pcd, voxel_size):
+    radius_normal = voxel_size * 2
+    radius_feature = voxel_size * 5
+    pcd.estimate_normals(open3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+    pcd_fpfh = open3d.pipelines.registration.compute_fpfh_feature(pcd, open3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_fpfh
 
-    source = FCGF.get_features(src_feature_file, pcd_only=True)
-    target = FCGF.get_features(tgt_feature_file, pcd_only=True)
+
+def ransac_feature_matching(source, target, source_feat, target_feat, n_ransac, threshold, p2p=True):
+    if p2p:
+        estimation_method = open3d.pipelines.registration.TransformationEstimationPointToPoint(False)
+    else:   
+        estimation_method = open3d.pipelines.registration.TransformationEstimationPointToPlane()
     
-    source, source_fpfh = compute_fpff(source, voxel_size, down_sample=False)
-    target, target_fpfh = compute_fpff(target, voxel_size, down_sample=False)
+    return open3d.pipelines.registration.registration_ransac_based_on_feature_matching(
+        source, target, source_feat, target_feat, True,
+        threshold, estimation_method, n_ransac,
+        [
+            open3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
+            open3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(threshold)
+        ],
+        open3d.pipelines.registration.RANSACConvergenceCriteria(100000, 0.999))
     
-    source.paint_uniform_color(np.random.random(3).tolist())
     
-    global_reg = exec_global_reg(source, target, source_fpfh, target_fpfh, n_ransac=4, threshold=0.05)
-    local_reg = exec_local_reg(source, target, threshold=0.05, trans_init=global_reg.transformation, max_iteration=30)
-    
-    return source, target, local_reg
+def view(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    open3d.visualization.draw_geometries([source_temp, target_temp])
