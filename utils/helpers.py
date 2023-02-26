32b1800e0ee8c4183fe2cc7127b01de8685f78dc
Darshana Rathnayake <darshanakgr@gmail.com>
2023-02-27 07:26:11 +0800

    new method

--- utils/helpers.py
+++ utils/helpers.py
@@ -87,3 +87,19 @@ def inv_transform(T):
     T_inv[:3, :3] = T[:3, :3].T
     T_inv[:3, 3] = -np.dot(T[:3, :3].T, T[:3, 3])
     return T_inv
+
+
+def rotation_matrix_to_euler_angles(R):
+    sy = np.sqrt(R[0,0]**2 + R[1,0]**2)
+    singular = sy < 1e-6
+
+    if not singular:
+        x = np.arctan2(R[2,1], R[2,2])
+        y = np.arctan2(-R[2,0], sy)
+        z = np.arctan2(R[1,0], R[0,0])
+    else:
+        x = np.arctan2(-R[1,2], R[1,1])
+        y = np.arctan2(-R[2,0], sy)
+        z = 0
+
+    return np.array([x, y, z])
